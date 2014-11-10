23cccd948bde95111e6c86f1457bfccac963d634
julitamusial <musial.julita@gmail.com>
2014-11-10 19:47:55 +0100

    dodanie formularza

--- script.js
+++ script.js
@@ -1,40 +1,48 @@
 $(document).ready(function(){
-	$('.form-button').click(function(){
-		$('.form').toggle();
+	$('.submit-button').click(function(){
+		$('#form').toggle();
 	});
 	
-	/*Formularz*/
-	$('.f-button').click(function(){
-		var addImie = $('#fname').val();
-		var addNazw= $('#lname').val();
-		var addEmail = $('#email').val();
-		var addMess = $('#message').val().length;
-		var charactersLeft = 140 - addMess; 
+//Sprawdzanie pól formularza//	
+	$("#submit").click(function() {
+		var isValid = true;
+		$('input[type="text"], input[type="email"]').each(function(){
+			if ($.trim($(this).val()) ==''){
+				isValid = false;
+				$(this).addClass('empty');
+		}else {
+				$(this).removeClass('empty');	
+		}		
+		});
 		
-		if(addImie === ""){
-			$('#r-fname').addClass('empty');
-		}else{
-			$('#r-fname').removeClass('empty');
-		}
-		if(addNazw === ""){
-			$('#r-lname').addClass('empty');	
-		}else{
-			$('#r-lname').removeClass('empty');
-		}
-		if(addEmail === ""){
-			$('#r-email').addClass('empty');	
-		}else{
-			$('#r-email').removeClass('empty');
-		};
-		if(addMess === ""){
-			$('#r-mess').addClass('empty');	
-		}else if (charactersLeft == 140){
-			$('.f-button').addClass('empty');
-		}
-		else{
-			$('#r-mess').removeClass('empty');
-		}
-		
-	$('input[type=text]').val('');
-	
+		if (isValid == true){
+			$('input[type="text"], input[type="email"], textarea').fadeOut('slow').val('');
+			$("#submit").val("Twoje zgłoszenie zostało wysłane");
+	}
+	});
 });
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+/*		var name = $("#name").val();
+		var email = $("#email").val();
+		var message = $("#message").val();
+		var contact = $("#contact").val();
+		
+		$("#returnmessage").empty(); // To empty previous error/success message.
+
+		// Checking for blank fields.
+		if (name == '') {
+			$("#name").addClass('empty');
+		} else {
+			$("#name").removeClass('empty');
+		}
+});*/
