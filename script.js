6cecf7adee67baf96796811d96970a10b166790d
julitamusial <musial.julita@gmail.com>
2014-11-11 15:15:33 +0100

    Dodanie efektów na stronie

--- script.js
+++ script.js
@@ -2,7 +2,31 @@ $(document).ready(function(){
 	$('.submit-button').click(function(){
 		$('#form').toggle();
 	});
+
+//Powiększanie ikonek społecznościowych
+	$('.social').hover(function(){
+		$(this).animate({
+			width: "42px",
+			height: "42px"
+		});
+		}, function(){
+		$(this).animate({
+			width: "32px",
+			height: "32px"
+		});
+		});
+			
+
+//Powiększanie submit button//
+	$('.submit-button').hover(
+	function(){
+		$(this).addClass('active');
+	}, function() {
+		$(this).removeClass('active');
+	}
+	);
 	
+
 //Sprawdzanie pól formularza//	
 	$("#submit").click(function() {
 		var isValid = true;
@@ -20,29 +44,62 @@ $(document).ready(function(){
 			$("#submit").val("Twoje zgłoszenie zostało wysłane");
 	}
 	});
-});
+	
+//Obrazki
+	$('.image').hover(function(){
+		$(this).animate({opacity: "0.7"}, 500);
+	}, function(){
+		$(this).animate({opacity: "1"}, 500);
+		});
 	
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-/*		var name = $("#name").val();
-		var email = $("#email").val();
-		var message = $("#message").val();
-		var contact = $("#contact").val();
-		
-		$("#returnmessage").empty(); // To empty previous error/success message.
 
-		// Checking for blank fields.
-		if (name == '') {
-			$("#name").addClass('empty');
-		} else {
-			$("#name").removeClass('empty');
-		}
-});*/
+	
+//Galeria//
+ $('#checkbox').change(function(){
+    setInterval(function () {
+        moveRight();
+    }, 3000);
+  });
+  
+	var slideCount = $('#slider ul li').length;
+	var slideWidth = $('#slider ul li').width();
+	var slideHeight = $('#slider ul li').height();
+	var sliderUlWidth = slideCount * slideWidth;
+	
+	$('#slider').css({ width: slideWidth, height: slideHeight });
+	
+	$('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+	
+    $('#slider ul li:last-child').prependTo('#slider ul');
+
+    function moveLeft() {
+        $('#slider ul').animate({
+            left: + slideWidth
+        }, 200, function () {
+            $('#slider ul li:last-child').prependTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+    function moveRight() {
+        $('#slider ul').animate({
+            left: - slideWidth
+        }, 200, function () {
+            $('#slider ul li:first-child').appendTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+    $('p.control_prev').click(function () {
+        moveLeft();
+    });
+
+    $('p.control_next').click(function () {
+        moveRight();
+    });
+
+	
+	});
+	
+
