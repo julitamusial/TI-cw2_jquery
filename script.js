d8a6dd15b22df4bc5b331af4edbc032f531e1bc6
julitamusial <musial.julita@gmail.com>
2014-11-12 01:28:11 +0100

    Dodanie

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 $(document).ready(function(){
+	
+//Link menu icon
+	$('#Link').hover(function(){
+		$(this).css({
+			"background-color": "#CB1F55"},
+		$(this).children().css({
+			"color": "#ECF0F1"
+		}));		
+	}, function(){
+		$(this).css({"background-color": "none"},
+		$(this).children().css({
+			"color": "none"
+		}));		
+	});
+	
+//Submit button
 	$('.submit-button').click(function(){
 		$('#form').toggle();
 	});
 
-//Powiększanie ikonek społecznościowych
+//Styling submit button
+	$('.submit-button').hover(function(){
+		$(this).addClass('active');
+	}, function() {
+		$(this).removeClass('active');
+	}
+	);
+	
+//Social icons - animation
 	$('.social').hover(function(){
 		$(this).animate({
-			width: "42px",
-			height: "42px"
+			width: "40px",
+			height: "40px"
 		});
 		}, function(){
 		$(this).animate({
@@ -16,46 +40,88 @@ $(document).ready(function(){
 		});
 		});
 			
+//Dynamic page load "page2.html"
+	$('#Link').click(function(){
+		$('body').load("page2.html");
+	});
 
-//Powiększanie submit button//
-	$('.submit-button').hover(
-	function(){
-		$(this).addClass('active');
-	}, function() {
-		$(this).removeClass('active');
-	}
-	);
+//Adding post on subpage "page2.html"
+	$('.form-send-button').click(function(event){
+		var post = $('.status-box').val();
+		$('<li>').text(post).prependTo('.posts');
+		$('.status-box').val('');
+		$('.counter').text(140);
+		$('.form-send-button').addClass('disabled');
+		
+	});
 	
+	$('.status-box').keyup(function(event) {
+		var postLength = $(this).val().length;
+		var charLeft = 140 - postLength;
+		$('.counter').text(charLeft);
+		
+		if(charLeft < 0){
+			$('.form-send-button').addClass('disabled');
+		}
+		else if(charLeft === 140){
+			$('.form-send-button').addClass('disabled');
+		}
+		else {
+		$('.form-send-button').removeClass('disabled');
+		}
+	});
+		$('.form-send-button').addClass('disabled');
 
-//Sprawdzanie pól formularza//	
+
+//Checking empty values in form
 	$("#submit").click(function() {
 		var isValid = true;
 		$('input[type="text"], input[type="email"]').each(function(){
 			if ($.trim($(this).val()) ==''){
 				isValid = false;
 				$(this).addClass('empty');
-		}else {
+				alert("Please enter all the required information");
+			}else {
 				$(this).removeClass('empty');	
 		}		
 		});
-		
 		if (isValid == true){
 			$('input[type="text"], input[type="email"], textarea').fadeOut('slow').val('');
-			$("#submit").val("Twoje zgłoszenie zostało wysłane");
-	}
+			$("#submit").val("Great job!");
+		}
+	});
+
+//ReadMore icons
+	$(".read-more").hover(function(){
+		$(this).css({
+			"background-color": "#3A3D8F",
+			"border-color": "white"});
+		$(this).children().css(
+			"color", "white"
+		);
+	}, function(){
+		$(this).css({
+			"background-color": "transparent",
+			"border-color": "#3A3D8F"});
+		$(this).children().css(
+			"color", "#3A3D8F"
+		);
 	});
 	
-//Obrazki
+//Images on page
 	$('.image').hover(function(){
 		$(this).animate({opacity: "0.7"}, 500);
 	}, function(){
 		$(this).animate({opacity: "1"}, 500);
 		});
 	
-		
+//Gallery appearing
+	$('.break').click(function(){
+		$('.g-container').fadeIn();
+	});		
 
 	
-//Galeria//
+//Slide gallery
  $('#checkbox').change(function(){
     setInterval(function () {
         moveRight();
@@ -76,7 +142,7 @@ $(document).ready(function(){
     function moveLeft() {
         $('#slider ul').animate({
             left: + slideWidth
-        }, 200, function () {
+        }, 400, function () {
             $('#slider ul li:last-child').prependTo('#slider ul');
             $('#slider ul').css('left', '');
         });
@@ -85,21 +151,20 @@ $(document).ready(function(){
     function moveRight() {
         $('#slider ul').animate({
             left: - slideWidth
-        }, 200, function () {
+        }, 400, function () {
             $('#slider ul li:first-child').appendTo('#slider ul');
             $('#slider ul').css('left', '');
         });
     };
 
-    $('p.control_prev').click(function () {
+    $('p.arrow_prev').click(function () {
         moveLeft();
     });
 
-    $('p.control_next').click(function () {
+    $('p.arrow_next').click(function () {
         moveRight();
     });
-
 	
-	});
+});
 	
 
